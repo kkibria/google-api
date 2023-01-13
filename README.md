1e1465ef6f4cce606737c98e95e56e98e2058923
Khan Kibria <khan.kibria@gmail.com>
2023-01-13 08:19:26 -0800

    update

--- README.md
+++ README.md
@@ -33,3 +33,24 @@ AI generated videos can be uploaded to youtube using this card from colab.
 ## text to speech
 run `txt2spc.py`
 
+
+### available voices
+
+<https://cloud.google.com/text-to-speech/docs/voices>
+
+| Language     | Voice type | Voice name      | SSML Gender
+| -------------|------------|-----------------|------------
+| English (US) | WaveNet    | en-US-Wavenet-A | MALE	
+| English (US) | WaveNet    | en-US-Wavenet-B | MALE	
+| English (US) | WaveNet    | en-US-Wavenet-C | FEMALE	
+| English (US) | WaveNet    | en-US-Wavenet-D | MALE	
+| English (US) | WaveNet    | en-US-Wavenet-E | FEMALE	
+| English (US) | WaveNet    | en-US-Wavenet-F | FEMALE	
+| English (US) | WaveNet    | en-US-Wavenet-G | FEMALE	
+| English (US) | WaveNet    | en-US-Wavenet-H | FEMALE	
+| English (US) | WaveNet    | en-US-Wavenet-I | MALE	
+| English (US) | WaveNet    | en-US-Wavenet-J | MALE  
+
+## speech shaping
+
+<https://cloud.google.com/text-to-speech/docs/ssml>
--- txt2spc.py
+++ txt2spc.py
@@ -6,24 +6,38 @@ Note: ssml must be well-formed according to:
 """
 from google.cloud import texttospeech
 from lib import get_tokens, CLOUD_PLATFORM_SCOPE
-
+ 
 credentials = get_tokens(fetch=True, scope=CLOUD_PLATFORM_SCOPE)
 
 # Instantiates a client
 client = texttospeech.TextToSpeechClient(credentials=credentials)
 
 # Set the text input to be synthesized
-synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+# <prosody rate="x-slow" pitch="+5st">
+# </prosody>
+
+ssml = """<emphasis level="reduced">
+Brad sings of love, Ana's heart will sway,
+Three years strong, and forever to stay,
+First date spot, where he'll get down on one knee,
+Ana, my love, will you marry me?"
+</emphasis>"""
+
+synthesis_input = texttospeech.SynthesisInput(ssml=ssml)
+# synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
 
 # Build the voice request, select the language code ("en-US") and the ssml
 # voice gender ("neutral")
 voice = texttospeech.VoiceSelectionParams(
-    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.MALE,
+    name="en-US-Wavenet-I"
 )
 
 # Select the type of audio file you want returned
 audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3
+    audio_encoding=texttospeech.AudioEncoding.MP3,
+    # effects_profile_id="headphone-class-device",
+    speaking_rate=1.0
 )
 
 # Perform the text-to-speech request on the text input with the selected
